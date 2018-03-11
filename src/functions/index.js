fbffda2005d37f8b35884392f8ab9158775cd511
Ivan Olivares <ivan.olivares@globant.com>
2018-03-11 18:18:15 -0300

    Update names and nextjs version

--- src/functions/index.js
+++ src/functions/index.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev, conf: { distDir: 'next' } })
 const handle = app.getRequestHandler()
 
-exports.next = functions.https.onRequest((req, res) => {
+exports.ssrdotcom = functions.https.onRequest((req, res) => {
   console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
   return app.prepare().then(() => handle(req, res))
 })
