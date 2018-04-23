bc71e736a33a7eb0f83e95de9f1572bad5f6601f
Ivan Olivares <ivan.olivares@globant.com>
2018-04-23 02:23:31 -0300

    Some design corrections :nail_care:

--- app/pages/index.js
+++ app/pages/index.js
@@ -18,11 +18,11 @@ export default () =>
         <div className="col-xs-12">
           <section className="hero">
             <div className="hero__content">
-              <h1 className="hero__content-title">Hey, I'm <strong>Iván Olivares Rojas</strong>, from Santiago, Chile.</h1>
+              <h1 className="hero__content-title">Hey, I'm <strong>Iván Olivares Rojas</strong></h1>
               <div className="hero__content-text">
                 <p className="extra-large">I'm focused on building beautiful interfaces and experiences for customers.</p>
-                <p className="large secondary-text">Currently work as Front-end software developer at <a className="globant" href="http://www.globant.com" target="_blank">Globant</a>.</p>
-                <p className="large secondary-text">Also I'm an amateur photographer, a enthusiast community builder and volunteer for non-profit organizations.</p>
+                <p className="large secondary-text">Currently work as Front-end software developer at <a className="globant" href="http://www.globant.com" target="_blank">Globant</a> in Santiago, Chile.</p>
+                <p className="large secondary-text">Also I'm an amateur photographer, a enthusiast community builder<br />and volunteer for non-profit organizations.</p>
               </div>
               <div className="hero__content-action">
                 <p className="secondary-text">
