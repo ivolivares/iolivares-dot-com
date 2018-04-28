e09d83ac5eb77d0f2dfd97b0d5bf6c0179c52e08
Ivan Olivares <ivan.olivares@globant.com>
2018-04-28 18:46:53 -0300

    Update sw and readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iolivares.com
-www.iolivares.com project :)
+
+This is my personal website, I use:
+
+· Next.JS
+· Express.JS
+· Firebase
+· Flexboxgrid
+## Licenses
+
+Icons Brands Flat 2 by Swifticons using a Creative Commons (Attribution 3.0 Unported) license: https://www.iconfinder.com/iconsets/brands-flat-2
--- app/service-worker.js
+++ app/service-worker.js
@@ -86,7 +86,16 @@ self.addEventListener('fetch', (event) => {
     }
   }
 
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  )
+  event.respondWith(async () => {
+    // Try to get the response from a cache.
+    const cachedResponse = await caches.match(event.request)
+    // Return it if we found one.
+    if (cachedResponse) return cachedResponse
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request)
+  })
+
+  // event.respondWith(
+  //   caches.match(event.request).then((response) => response || fetch(event.request))
+  // )
 })
