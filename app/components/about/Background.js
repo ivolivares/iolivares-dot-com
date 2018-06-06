5cd9ca3e00ef973d169aa28312241367c4a08fbc
Ivan Olivares <ivan.olivares@globant.com>
2018-06-05 22:40:07 -0400

    Changes in texts on about section

--- app/components/about/Background.js
+++ app/components/about/Background.js
@@ -4,11 +4,11 @@ export default () =>
   <section className="section">
     <div className="section__title">Background</div>
     <div className="section__content">
-      <p>I have more than 6 years of experience. In recent years I've worked with such technologies as: JavaScript, NodeJS, mobile development (native and hybrid for iOS), SQL and NoSQL databases and different JavaScript libraries and frameworks like: AngularJS, ReactJS, BackboneJS, ExpressJS, Restify, among others.</p>
-      <p>Also, in last years I've worked in agile methodologies, proud of being part of <strong>excellence teams</strong> where I'm outstanding by my commitment, leadership, focus in objectives and global vision.</p>
-      <p><strong>I like challenging me every time</strong>, based on this, on my career I've had different experiences with different industries: digital marketing, private and public events with high concurrency (online video and audio streaming) and recently in airline industry.</p>
-      <p>When I'm not in front of a computer screen, I'm probably photographing some portraits or landscapes, <strong>thinking some ideas</strong> to change the world, or crossing off another item on my bucket list.</p>
-      <p>Would you like know more? You can contact me and ask what you need to know. <strong>You can also <a className="underline-link" href="https://www.paypal.me/ivanolivaresrojas/4" rel="noopener" title="Buy me a coffee!">buy me a coffee</a></strong>, are always welcome :)</p>
+      <p>I have more than 6 years of work experience. In recent years I've worked with such technologies as: JavaScript, NodeJS, mobile development (native and hybrid for iOS), SQL and NoSQL databases and different JavaScript libraries and frameworks like: AngularJS, ReactJS, BackboneJS, ExpressJS, Restify, among others.</p>
+      <p>Also, in the last few years I've worked with agile methodologies, proud of being part of <strong>excellence teams</strong> where I'm outstanding for my commitment, leadership, focus on objectives and global vision.</p>
+      <p><strong>I like challenging myself all the time</strong>. Based on this, my career has been filled with different experiences in different industries: digital marketing, private and public events with high concurrency (online video and audio streaming) and recently the airline industry.</p>
+      <p>When I'm not in front of a computer screen, I'm probably photographing some portraits or landscapes, <strong>thinking of some ideas</strong> to change the world, or crossing off another item from my bucket list.</p>
+      <p>Would you like to know more? You can contact me and ask what you need to know. <strong>You can also <a className="underline-link" href="https://www.paypal.me/ivanolivaresrojas/4" rel="noopener" title="Buy me a coffee!">buy me coffee</a></strong> which is welcome :)</p>
       <a className="arrow-link" href="/resume.pdf" rel="noopener" target="_blank">View My Resume</a>
     </div>
   </section>
--- app/components/about/index.js
+++ app/components/about/index.js
@@ -7,11 +7,11 @@ export default () =>
       <div className="col-xs-12">
         <div className="hero">
           <div className="hero__content">
-            <h1 className="hero__content-title">Hey, I'm <strong>Iván Olivares Rojas</strong></h1>
+            <h1 className="hero__content-title">Hey! I'm <strong>Iván Olivares Rojas</strong></h1>
             <div className="hero__content-text">
               <p className="extra-large">I'm focused on building beautiful interfaces and experiences for customers.</p>
-              <p className="large secondary-text">Currently work as Front-end software developer at <a className="globant" href="http://www.globant.com" rel="noopener" target="_blank">Globant</a> from Santiago, Chile.</p>
-              <p className="large secondary-text">Also I'm an amateur photographer, a enthusiast community builder<br />and volunteer for non-profit organizations.</p>
+              <p className="large secondary-text">Currently working as Front-end software developer at <a className="globant" href="http://www.globant.com" rel="noopener" target="_blank">Globant</a> in Santiago, Chile.</p>
+              <p className="large secondary-text">I'm also an amateur photographer, a community builder enthusiast<br />and I volunteer for non-profit organizations.</p>
             </div>
             <div className="hero__content-action">
               <p className="secondary-text">
