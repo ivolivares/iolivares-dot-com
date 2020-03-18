88cc540101e123815222170d9419383818040f0c
Ivan Olivares <ivan.olivares@globant.com>
2020-03-18 12:46:48 -0300

    :space_invader: Updating service worker

--- public/sw.js
+++ public/sw.js
@@ -3,9 +3,9 @@ const CACHE_NAME = 'iodotcom'
 // Assesto catche
 const assetsToCache = [
   '/static/styles/main.css',
-  '/_next/cB1N3FpNU6iJ8y~hgOnq1/page/_error.js',
-  '/_next/cB1N3FpNU6iJ8y~hgOnq1/page/index.js',
-  '/_next/cB1N3FpNU6iJ8y~hgOnq1/page/about.js',
+  '/_next/qNDxBW7Ipue1nBZgA7t~w/page/_error.js',
+  '/_next/qNDxBW7Ipue1nBZgA7t~w/page/index.js',
+  '/_next/qNDxBW7Ipue1nBZgA7t~w/page/about.js',
   '/static/images/me.jpg',
   '/static/images/logo-sprite.jpg',
   '/static/images/emojis/technologist.png',
