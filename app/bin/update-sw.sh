66ff84537b4330524cb7a0e3cca6471d1b871ab4
Ivan Olivares <ivan.olivares@globant.com>
2020-06-15 09:59:00 -0400

    Update-sw message

--- app/bin/update-sw.sh
+++ app/bin/update-sw.sh
@@ -3,5 +3,6 @@
 echo '>> Publishing on GIT the new service-worker...'
 cd ../
 git add public/sw.js
-git commit -m ':space_invader: Updating service worker'
+# git commit -m ':space_invader: Updating service worker'
+git commit -m ":space_invader: $(curl -s http://whatthecommit.com/index.txt)"
 git push origin master
