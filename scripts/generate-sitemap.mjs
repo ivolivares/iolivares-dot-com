0feab72956919d9b43f2e047c4d40c8d0565d736
Iván Olivares R <hola@iolivares.com>
2021-12-07 14:25:51 -0300

    Sitemap fix (#24)
    * Fixing security policy in this branch because is security!
    
    * Fix sitemap builder, and improve to escalate

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -9,39 +9,45 @@ import prettier from 'prettier'
 async function generateSitemap() {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const pages = await globby([
-    'pages/*.js',
-    'data/**/*.mdx',
+    'data/pages/*.mdx',
+    '!pages/*.js',
     '!data/*.mdx',
-    '!data/failover',
+    '!data/pages/failover',
     '!pages/_*.js',
     '!pages/api',
     '!pages/404.js',
   ])
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${['es', ''].map((lang) => (
-              pages
-                .map((page) => {
-                  const path = page
-                    .replace('pages', '')
-                    .replace('data', '')
-                    .replace('.js', '')
-                    .replace('.mdx', '')
-                  const pathRoute = path === '/index' ? '' : path
-                  const route = lang === '' ? pathRoute : `/${lang}${pathRoute}`
+  const siteContent = new Set()
 
-                  return `
-                        <url>
-                            <loc>${`https://iolivares.com${route}`}</loc>
-                        </url>`
-                })
-                .join('')
-                
-            )).join('')}
-        </urlset>
-    `
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${['', 'es'].map((lang) => {
+          let sitemapContent = ''
+          sitemapContent += `
+            <url>
+                <loc>${`https://iolivares.com${(lang === '' ? '' : `/${lang}`)}`}</loc>
+            </url>`
+
+          sitemapContent += pages.map((page) => {
+            const path = page.replace(/(pages\/)|(data\/)|(\.js)|(\.mdx)/g, '')
+            const pathRoute = (path === '/index' ? '' : path).replace(/(\.en)|(\.es)/g, '')
+            const route = lang === '' ? `/${pathRoute}` : `/${lang}/${pathRoute}`
+
+            if (!siteContent.has(route)) {
+              siteContent.add(route)
+
+              return `
+                  <url>
+                      <loc>${`https://iolivares.com${route}`}</loc>
+                  </url>`
+            }
+          }).join('')
+
+          return sitemapContent
+        }).join('')}
+    </urlset>`
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
