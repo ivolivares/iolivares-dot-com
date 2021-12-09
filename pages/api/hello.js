6104bbaecb84c57f8ae983538cf971376f4360fa
Iván Olivares R <hola@iolivares.com>
2021-12-09 00:22:54 -0300

    Updating sentry version and adding some updates to README (#26)

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from "@sentry/nextjs"
 
 const Hello = (_req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
 
-export default Hello
+export default withSentry(Hello)
